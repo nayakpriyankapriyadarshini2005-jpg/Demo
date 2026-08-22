2a9d93011398c5ff0c5769ed719a41e97b28f64a
Priyanka <nayakpriyankapriyadarshini2005@gmail.com>
2026-08-22 10:14:23 +0530

    some changes

--- 04 javascript/lec15-DOM Events/script.js
+++ 04 javascript/lec15-DOM Events/script.js
@@ -7,80 +7,26 @@
 // // const h1 = document.getElementById("first-heading")
 // // console.log(h1)
 
-// // const handleDblClick = () => {
-// //     console.log("Heading double clicked");
-// //     }
-// // //console.log(handleDblClick);
-
-// // //using event properties
-
-// // const h1 = document.getElementById("first-heading")
-// // console.log(h1);
-
-// // h1.onmouseover = handleOnMouseOver
-
-// // function handleOnMouseOver() {
-// //     console.log("Mouse hover effect - 2nd fun");
-    
-// // }
-
-// // Using addEventListener
 // const handleDblClick = () => {
-//   console.log("Heading double clicked")
-// }
+//     console.log("Heading  clicked");
+//     }
+// console.log(handleDblClick);
+
+// const handleDblClick2 = () => {
+//     console.log("Heading double clicked");
+//     }
+// console.log(handleDblClick2);
 
 
-// // 2. using event properties
-// const h1 = document.getElementById("first-heading")
 
-// h1.onmouseover = handleOnMouseOver
-// function handleOnMouseOver() {
-//   console.log("Mouse hover effect")
-// }
-
-// h1.onmouseover = handleOnMouseOver2
-// function handleOnMouseOver2() {
-//   console.log("Mouse hover effect - 2nd fun")
-// }
-
-// // 3. using addEventListener
-// const secondH1 = document.querySelector("#sec-heading")
-
-// secondH1.addEventListener("click", () => {
-//   console.log("DOM Event Listener clicked")
-// })
-
-// secondH1.addEventListener("click", handleDomEventHeadingClick)
-// // function handleDomEventHeadingClick(event){
-// //   console.log("DOM Event Listener clicked - 2nd fun")
-// //   console.log("///////////", event)
-// //   console.log("x co-ordinate", event.clientX)
-// //   console.log("y co-ordinate", event.clientY)
-// //   console.log("event type", event.type)
-// //   console.log("event target", event.target)
-// //   console.log("event target text", event.target.innerText)
-// //   // event.target.style.color = 'green'
-// //   event.target.remove()
-// // }
-// function handleDomEventHeadingClick(e){
-//   console.log("DOM event Listener clicked - 2nd fun")
-//   console.log("///////////", e)
-//   console.log("x co-ordinate", e.clientX)
-//   console.log("y co-ordinate", e.clientY)
-//   console.log("e type", e.type)
-//   console.log("e target", e.target)
-//   console.log("e target text", e.target.innerText)
-//   // e.target.style.color = green'
-//   e.target.remove()
-// }
-
+///========REAL CODE=======
 
 const handleDblClick = () => {
  // console.log("Heading double clicked")
 }
 
 
-// 2. using event properties
+//2. using event properties
 const h1 = document.getElementById("first-heading")
 
 h1.onmouseover = handleOnMouseOver
@@ -99,31 +45,31 @@ const secondH1 = document.querySelector("#sec-heading")
 
 secondH1.addEventListener("click", () => {
   console.log("DOM Event Listener clicked")
-})
+ })
 
 secondH1.addEventListener("click", handleDomEventHeadingClick)
-// function handleDomEventHeadingClick(event){
-//   console.log("DOM Event Listener clicked - 2nd fun")
-//   console.log("///////////", event)
-//   console.log("x co-ordinate", event.clientX)
-//   console.log("y co-ordinate", event.clientY)
-//   console.log("event type", event.type)
-//   console.log("event target", event.target)
-//   console.log("event target text", event.target.innerText)
-//   event.target.style.color = 'green'
-//   //event.target.remove()
-// }
-function handleDomEventHeadingClick(e){
-  console.log("DOM e Listener clicked - 2nd fun")
-  console.log("///////////", e)
-  console.log("x co-ordinate", e.clientX)
-  console.log("y co-ordinate", e.clientY)
-  console.log("e type", e.type)
-  console.log("e target", e.target)
-  console.log("e target text", e.target.innerText)
-  e.target.style.color = 'pink'
-  //e.target.remove()
+ function handleDomEventHeadingClick(event){
+   console.log("DOM Event Listener clicked - 2nd fun")
+  console.log("///////////", event)
+  console.log("x co-ordinate", event.clientX)
+  console.log("y co-ordinate", event.clientY)
+  console.log("event type", event.type)
+  console.log("event target", event.target)
+  console.log("event target text", event.target.innerText)
+  event.target.style.color = 'green'
+  //event.target.remove()
 }
+// function handleDomEventHeadingClick(e){
+//   console.log("DOM e Listener clicked - 2nd fun")
+//   console.log("///////////", e)
+//   console.log("x co-ordinate", e.clientX)
+//   console.log("y co-ordinate", e.clientY)
+//   console.log("e type", e.type)
+//   console.log("e target", e.target)
+//   console.log("e target text", e.target.innerText)
+//   e.target.style.color = 'pink'
+//   //e.target.remove()
+// }
 
 
 
